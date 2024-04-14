57fd5bb593d6ebfc3d0de8d1463772353811dcda
Shreya Bagchi <106909868+BagchiShreya@users.noreply.github.com>
2024-04-14 14:43:09 +0530

    Time: 4 ms (56.80%), Space: 44.7 MB (38.95%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -6,18 +6,18 @@ class Solution {
                 newStr.append(Character.toLowerCase(s.charAt(i)));
             }
         }
-        System.out.println("new String: " + newStr);
+        //System.out.println("new String: " + newStr);
         StringBuilder revstr = new StringBuilder();
         for (int i = newStr.length() - 1; i >= 0; i--) {
             revstr.append(newStr.charAt(i));
         }
         boolean palindrome = false;
         if (revstr.toString().equals(newStr.toString())) {
-            palindrome = true;
+            return true;
         } else {
-            palindrome = false;
+            return false;
         }
 
-        return palindrome;
+        //return palindrome;
     }
 }
