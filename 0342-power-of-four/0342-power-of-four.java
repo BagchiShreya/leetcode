b8bff6da2ef61ee6f6360e22c28dadec0088ceb7
Shreya Bagchi <106909868+BagchiShreya@users.noreply.github.com>
2024-09-09 21:28:23 +0530

    Time: 1 ms (27.44%), Space: 40.7 MB (48.42%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -3,7 +3,7 @@ class Solution {
         if(n<0)
             return false;
         int flag=0;
-        for(int i=0;i<=16;i++){
+        for(int i=0;i<=15;i++){
             if(n==Math.pow(4,i)){
                 return true;
             }
