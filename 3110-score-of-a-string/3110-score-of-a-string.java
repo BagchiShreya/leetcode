4594e49e9ec1dc82aeebf5a9aae87b90eaad3c43
Shreya Bagchi <106909868+BagchiShreya@users.noreply.github.com>
2024-09-23 14:46:18 +0530

    Time: 1 ms (99.82%), Space: 41.8 MB (84.21%) - LeetHub

---
+++ 3110-score-of-a-string/3110-score-of-a-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int scoreOfString(String s) {
+        int sum=0,diff=0;
+        char ch[]=s.toCharArray();
+        for(int i=0;i<ch.length-1;i++){
+            diff=Math.abs((int)ch[i]-(int)ch[i+1]);
+            sum+=diff;
+        }
+        return sum;
+    }
+}
