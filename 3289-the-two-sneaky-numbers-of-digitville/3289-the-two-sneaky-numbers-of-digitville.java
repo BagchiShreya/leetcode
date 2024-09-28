4b77dacfaaff1583724de965cb2b3a25a818b984
Shreya Bagchi <106909868+BagchiShreya@users.noreply.github.com>
2024-09-28 15:30:20 +0530

    Time: 2 ms (71.94%), Space: 44.8 MB (32.58%) - LeetHub

--- 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.java
+++ 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.java
@@ -1,8 +1,9 @@
 class Solution {
     public int[] getSneakyNumbers(int[] nums) {
         int[] res = new int[2];
+        int j=0;
         HashSet<Integer> set = new HashSet<>();
-        for(int i=0,j=0;i<nums.length;i++){
+        for(int i=0;i<nums.length;i++){
             if(!set.isEmpty() && set.contains(nums[i])){
                 res[j] = nums[i];
                 j++;
